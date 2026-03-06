e49c31a39e871a4c50f0127832d670b85d165660
bidawi media presse <elaziziyassir2@gmail.com>
2026-03-06 16:50:56 +0100

    cors resend fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/bloto-drive-booster-agence.png",
+        headers: [
+          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
